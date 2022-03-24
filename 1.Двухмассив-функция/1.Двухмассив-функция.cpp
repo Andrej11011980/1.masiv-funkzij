d4d363e96e11efe9c4ca2900ad82b5c883427d74
Андрей Боярский <an0982978490@outlook.com>
2022-03-24 12:23:18 +0200

    Добавьте файлы проекта.

---
+++ 1.Двухмассив-функция/1.Двухмассив-функция.cpp
@@ -0,0 +1,71 @@
+﻿//1.	Дан двумерный массив размерностью M х N, заполненный случайными числами из диапазона от 0 до 20. Определить сумму элементов массива, среднее арифметическое, минимальный и максимальный элемент.
+
+#include <iostream>
+#include <Windows.h>
+#include<time.h>
+using namespace std;
+const int Youchu = 4;
+const int Xox = 5;
+int Andr[Youchu][Xox];
+double sum = 0;
+void SetMaxelement() 
+{
+
+	for (int y = 0; y < Youchu; y++)
+	{
+
+		int max = Andr[y][0];
+		for (int x = 0; x < Xox; x++)
+		{
+			if (Andr[y][x] > max)
+				max = Andr[y][x];
+		}
+		cout << "Max element " << y << " row=" <<
+			max << endl;
+	}
+}
+void SetSumelement() {
+	for (int y = 0; y < Youchu; y++)
+	{
+		for (int x = 0; x < Xox; x++)
+		{
+
+			sum += Andr[y][x];
+		}
+		cout << "Summa=" << sum << endl;
+	}
+}
+void SetMinelement() {
+	int min = Andr[0][0];
+
+	for (int y = 0; y < Youchu; y++)
+	{
+
+
+		for (int x = 0; x < Xox; x++)
+		{
+			if (Andr[y][x] < min)
+				min = Andr[y][x];
+		}
+		cout << "Min element " << " " << min << endl;
+	}
+}
+int main()
+{
+	srand(time(0));
+	for (int y = 0; y < Youchu; y++)
+	{
+
+		for (int x = 0; x < Xox; x++)
+		{
+			Andr[y][x] = rand() % 20;
+			cout << Andr[y][x] << " | ";
+		}
+		cout << endl;
+	}
+	SetMaxelement();
+	SetSumelement();
+	SetMinelement();
+	return 0;
+}
+
